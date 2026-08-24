af246f840dc4843bea46b6c6745d3f395987c728
Oe <181693920+Oe0930@users.noreply.github.com>
2026-08-24 10:05:32 +0900

    ピン番号の修正

--- lib/SSR/pins.hpp
+++ lib/SSR/pins.hpp
@@ -1,20 +1,20 @@
 #pragma once
 
 // サーボ
-const uint8_t ETC_SERVO_PIN1 = 16;
-const uint8_t ETC_SERVO_PIN2 = 17;
-const uint8_t LIFT_SERVO_PIN = 18;
-const uint8_t BACK_SERVO_PIN = 19;
+const uint8_t ETC_SERVO_PIN1 = 32;
+const uint8_t ETC_SERVO_PIN2 = 34;
+const uint8_t LIFT_SERVO_PIN = 27;
+const uint8_t BACK_SERVO_PIN = 13;
 
 // リミットスイッチ
-const uint8_t LIMITSWITCH_PIN1 = 22;
-const uint8_t LIMITSWITCH_PIN2 = 23;
+//const uint8_t LIMITSWITCH_PIN1 = 22;
+//const uint8_t LIMITSWITCH_PIN2 = 23;
 
 // 駆動DC
 
 // 0 : 後輪 
 // 1 : 右前輪 
 // 2 : 左前輪 
-const uint8_t WHEEL_PWM_PINS[] = { 27, 32, 25 };
-const uint8_t WHEEL_DIR_PINS[] = { 14, 33, 26 };
+const uint8_t WHEEL_PWM_PINS[] = { 18, 16, 22 };
+const uint8_t WHEEL_DIR_PINS[] = { 17, 15, 21 };
 const uint8_t WHEEL_PWM_CHANNELS[] = { 3, 4, 5 };
